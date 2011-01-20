703fc052ee3ac149e908be48da64db4c91bcf485
Johannes Raggam <raggam-nl@adm.at>
2011-01-20 19:04:30 +0100

    API imports

---
+++ src/node/ext/template/__init__.py
@@ -0,0 +1,5 @@
+from node.ext.template._api import SectionedTemplate
+from node.ext.template._api import DTMLTemplate
+from node.ext.template._api import JinjaTemplate
+from node.ext.template._api import XMLTemplate
+from node.ext.template._api import ZPTemplate
